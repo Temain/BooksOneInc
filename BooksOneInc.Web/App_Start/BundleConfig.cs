50b521adcfd1951b12abf9523b373922061f9637
Асеев Артём <aseev@insystems.ru>
2019-08-31 17:28:51 +0300

    Datatables

--- BooksOneInc.Web/App_Start/BundleConfig.cs
+++ BooksOneInc.Web/App_Start/BundleConfig.cs
@@ -22,9 +22,15 @@ namespace BooksOneInc.Web
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
 					  "~/Scripts/bootstrap.js"));
 
+			bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+					  "~/Scripts/datatable/datatables.js"));
+
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
 					  "~/Content/site.css"));
+
+			bundles.Add(new StyleBundle("~/Content/datatables").Include(
+					  "~/Scripts/datatable/datatables.css"));
 		}
 	}
 }
